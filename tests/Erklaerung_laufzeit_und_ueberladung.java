f24463b03d8ccbcbaf28e82c7081695ee3085ea9
ssteinkellner <Sebastian Steinkellner>
2014-10-15 19:34:23 +0200

    branch testerei, erklärung laufzeit (halbfertig)

--- tests/Erklaerung_laufzeit_und_ueberladung.java
+++ tests/Erklaerung_laufzeit_und_ueberladung.java
@@ -6,7 +6,9 @@ import java.awt.Color;
 import oop.Astra;
 import oop.Opel;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author SSteinkellner
@@ -18,6 +20,9 @@ import org.junit.Before;
  * aufbauend auf {@link oop.tests.Erklaerung_typanpassung typanpassung}
  */
 public class Erklaerung_laufzeit_und_ueberladung {
+	Opel o, o2;
+	Astra a;
+	
 	/*
 	 * Laufzeitpolymorphie
 	 * während der Laufzeit kann man Objekte in übergeordnete Objekttypen hineinschreiben,
@@ -26,9 +31,42 @@ public class Erklaerung_laufzeit_und_ueberladung {
 	 * dementsprechend werden dann auch die methoden ausgeführt.
 	 */
 	
+	/**
+	 * hier werden ein opel und ein opel astra erstellt
+	 * so sieht es der compiler (deklarierter typ)
+	 * und so ist es in dem fall auch zur laufzeit (dynamischer typ)
+	 */
 	@Before
 	public void setup(){
-		Opel o = new Opel(2, Color.white);
-		Astra a = new Astra(22, 6, Color.red, true);
+		o = new Opel(2, Color.white);
+		a = new Astra(22, 6, Color.red, true);
+	}
+	
+	/**
+	 * hier packe ich jetzt den astra in einen opel.
+	 * da opel übergeordnet ist, ist das ohne probleme möglich.
+	 * man muss nur solange der astra verpackt ist auf die funktionen und parameter verzichten,
+	 * die astra mehr hat als opel
+	 * 
+	 * überschriebene methoden können verwendet werden, werden dann aber so ausgeführt,
+	 * wie es im innersten objekt definiert ist
+	 */
+	@Test
+	public void polymorphie01(){
+		o2 = (Opel) a;
+		
+		/* ich tanke in beide fahrzeuge 10 liter
+		 * der compiler denkt, das hier wären 2 opel
+		 */
+		o.tanken(10);
+		o2.tanken(10);
+
+		/* da das eine auto ein opel und das andere ein astra sind,
+		 * haben sie einen unterschiedlichen verbrauch auf der gleichen strecke
+		 */
+		o.drive(1200);
+		o2.drive(1200);
+		
+		Assert.assertFalse(o.getFuelLevel()!=o2.getFuelLevel());
 	}
 }
