d99b3a66ec73f54634aa4b12c252b7cfb07b1833
ssteinkellner <Sebastian Steinkellner>
2014-11-12 17:49:16 +0100

    implemented Calculator

--- ssteinkellner/calculators/Calculator.java
+++ ssteinkellner/calculators/Calculator.java
@@ -1,5 +1,7 @@
 package ssteinkellner.calculators;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import ssteinkellner.behavior.CalculationBehavior;
@@ -18,34 +20,52 @@ public class Calculator {
 	protected CalculationBehavior behavior;
 	
 	public Calculator(){
-		
+		values = new ArrayList<Double>();
+		modifier = 1.0;
+		behavior = null;
 	}
 	
 	public Calculator(CalculationBehavior cb){
-		
+		this();
+		behavior = cb;
 	}
 	
 	public void setCalculationBehavior(CalculationBehavior cb){
-		
+		behavior = cb;
 	}
 	
 	public void addValue(double value){
-		
+		values.add(value);
 	}
 	
 	public void removeValue(double value){
-		
+		if(values.contains(value)){
+			values.remove(value);
+		}
 	}
 	
 	public void setModifier(double modifier){
-		
+		this.modifier = modifier;
 	}
 	
 	public List<Double> prozessCalculations(){
-		return null;
+		if(behavior==null){
+			System.err.println("No calculationbehavior set. Aborting calculation!");
+			return null;
+		}
+		
+		List<Double> l = behavior.prozessCalculations(values, modifier);
+		values = l;
+		return l;
 	}
 	
 	public String toString(){
-		return null;
+		String text = "";
+		Iterator<Double> i = values.iterator();
+		while(i.hasNext()){
+			if(text.length()>0){ text+=", "; }
+			text+= ""+i.next();
+		}
+		return text;
 	}
 }
