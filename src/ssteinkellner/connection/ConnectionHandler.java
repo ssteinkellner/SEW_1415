082c8067af3f88169051c8229f0f2762ee2f6a91
ssteinkellner <Sebastian Steinkellner>
2015-01-06 17:12:53 +0100

    erste ausprogrammierte klassen, angefangener connectionhandler

--- src/ssteinkellner/connection/ConnectionHandler.java
+++ src/ssteinkellner/connection/ConnectionHandler.java
@@ -1,15 +1,43 @@
 package ssteinkellner.connection;
 
+import java.sql.Connection;
 
 /**
- * eine klasse, die fuer das auf/abbauen von verbindungen zustaendig ist
+ * eine klasse, die fuer das auf/abbauen von verbindungen zustaendig ist.
+ * es gibt maximal eine connection.
  * @author SSteinkellner
  * @version 2014.12.30
  */
 public class ConnectionHandler {
+	private Connection connection;
+	private UserCache usercache;
+	private String host, database;
 	
-	public ConnectionHandler(){
-		
+	public ConnectionHandler(UserCache userCache){
+		usercache = userCache;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getDatabase() {
+		return database;
 	}
 	
+	public Connection getConnection() {
+		if(connection==null){
+			
+		}
+		
+		return connection;
+	}
 }
