6ec339cba97daf6ddff2f842da05a4baa1c9b939
ssteinkellner <Sebastian Steinkellner>
2015-01-07 15:16:27 +0100

    arraytools erstellt

---
+++ src/ssteinkellner/tools/ArrayTools.java
@@ -0,0 +1,37 @@
+package ssteinkellner.tools;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * eine klasse mit methoden, um arrays in Strings umzuandeln
+ * @author SSteinkellner
+ * @version 2015.01.07
+ */
+public class ArrayTools {
+	/**
+	 * verwandelt eine string-liste in eine beistrichgetrennte auflistung als string
+	 * @param values liste der strings
+	 * @return eine beistrichgetrennte liste der strings
+	 */
+	public static String listToString(String[] values){
+		return listToString(values, ", ");
+	}
+	
+	/**
+	 * verwandelt eine string-liste in eine delimitergetrennte auflistung als string
+	 * @param values liste der strings
+	 * @param delimiter zeichen, das als trennzeichen verwendet werden soll
+	 * @return eine delimitergetrennte liste der strings
+	 */
+	public static String listToString(String[] values, String delimiter){
+		String text = "";
+		
+		for(int i=0;i<values.length;i++){
+			if(!text.isEmpty()){ text += delimiter; }
+			text += values[i];
+		}
+		
+		return text;
+	}
+}
