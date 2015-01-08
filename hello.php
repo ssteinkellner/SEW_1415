c4c26811adfbe221c1e374f603478fb30a9c5169
ssteinkellner <Sebastian Steinkellner>
2015-01-08 13:33:12 +0100

    headerkommentar

--- hello.php
+++ hello.php
@@ -1,1 +1,10 @@
-<?php echo 'Hello World!'; ?>
+<?php
+	/*
+		Name of file  : HalloWelt.c
+		Author        : Steinkellner Sebastian <ssteinkellner@student.tgm.ac.at>
+		Version       : 2015.01.08
+		Description   : a simple php file printing the text "Hello World!"
+	*/
+	
+	echo 'Hello World!';
+?>
