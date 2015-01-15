11e2be9fabff02f56833ff34a4d3398c55f10000
ssteinkellner <Sebastian Steinkellner>
2015-01-15 13:09:37 +0100

    aufgabe2 punkt2, properties erweitert

---
+++ test/Test_test.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Name of file  : HalloWelt.c
+	Author        : Steinkellner Sebastian <ssteinkellner@student.tgm.ac.at>
+	Version       : 2015.01.15
+	Description   : a phpunit test case to test the test.php from the elearning
+*/
+ 
+class Test extends PHPUnit_Framework_TestCase {
+    public function testZahl() {
+		$_GET['zahl'] = 5;
+		$this->expectOutputString('Hello World '.$i+42);
+		include '../test/';
+    }
+}
+
+?>
